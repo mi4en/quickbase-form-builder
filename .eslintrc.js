e97952f6586891920a30da86769508e5729ef5e0
Mario <marionikolov87@gmail.com>
2021-03-03 19:10:22 +0200

    add eslint rules; autoformat and change to crlf;

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/airbnb'],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'linebreak-style': ['error', 'windows'],
+		indent: [1, 'tab'],
+		indent: 'off',
+		'no-tabs': 0,
+		'no-return-assign': 0,
+		'arrow-parens': 0,
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
 };
